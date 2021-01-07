96dc01d4b816c1a7706ae676477c8f95903c1499
jiqinga <41818729+jiqinga@users.noreply.github.com>
2021-01-07 14:31:46 +0800

    :heart: 增加shc版本号

--- shc/shc.sh
+++ shc/shc.sh
@@ -3,6 +3,9 @@
 #Blog:https://jiqinga.github.io
 #Time:2021-01-07 11:29:27
 #Name:shc.sh
-wget http://7n.jiqinga.top/shc -O /usr/bin/shc
+wget http://7n.jiqinga.top/shc-4.0.3 -O /usr/bin/shc
 chmod +x /usr/bin/shc
-echo "shc -v -r -T -f xx.sh"
+echo "shc -v -r -f xx.sh -o xx"
+echo "设置过期时间以及过期输出信息
+shc -v -r -e 07/01/2021 -m '过期了'  -f a.sh -o a"
+
