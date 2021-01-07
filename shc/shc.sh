7e2bc6ad27693420ba66cb2ce5f1f8d2015bfbe2
jiqinga <41818729+jiqinga@users.noreply.github.com>
2021-01-07 14:07:32 +0800

    :bug: 更改错误的下载选项

--- shc/shc.sh
+++ shc/shc.sh
@@ -3,6 +3,6 @@
 #Blog:https://jiqinga.github.io
 #Time:2021-01-07 11:29:27
 #Name:shc.sh
-wget http://7n.jiqinga.top/shc -o /usr/bin/shc
+wget http://7n.jiqinga.top/shc -O /usr/bin/shc
 chmod +x /usr/bin/shc
 echo "shc -v -r -T -f xx.sh"
