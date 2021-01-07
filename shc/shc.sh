9ea6a8a32d4635c0b3a87f84c8deb9d8c2de054d
jiqinga <1758812645@qq.com>
2021-01-07 11:35:33 +0800

    :heart: 增加shc安装脚本

---
+++ shc/shc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Author:liuml
+#Blog:https://jiqinga.github.io
+#Time:2021-01-07 11:29:27
+#Name:shc.sh
+wget http://7n.jiqinga.top/shc -o /usr/bin/shc
+chmod +x /usr/bin/shc
+echo "shc -v -r -T -f xx.sh"
